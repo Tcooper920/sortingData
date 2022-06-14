50bc13478721f68827d9404e614e5a6c7849a1ea
Thomas Cooper <thomascooper@T-JR-MBP.fios-router.home>
2022-06-13 20:01:15 -0400

    Add search field functionality.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// "Database" of employees
 const employeeList = 
 {"employees":[  
     {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},  
@@ -12,12 +13,17 @@ const employeeList =
     {"name":"Aaron", "email":"Baaron@gmail.com"}, 
     {"name":"Charlie", "email":"charlie@gmail.com"}, 
     {"name":"Zack", "email":"Azack21@gmail.com"}, 
+    {"name":"Timmy", "email":"Timmy@gmail.com"}, 
+    {"name":"Jessica", "email":"jess@gmail.com"}, 
+    {"name":"Tim", "email":"Timmy@gmail.com"}, 
 ]};
 
 const employeeListContainer = document.getElementById("employee-list-container");
+const searchBar = document.getElementById("search-bar");
 const alphabeticalByNameRadioButton = document.getElementById("alphabetical-name");
 const alphabeticalByEmailRadioButton = document.getElementById("alphabetical-email");
 
+// Print employees to page
 const printEmployeesToPage = () => {
     employeeListContainer.innerHTML = "";
 	employeeList.employees.forEach(element => {
@@ -29,32 +35,44 @@ const printEmployeesToPage = () => {
 
 printEmployeesToPage();
 
-// Check for radio button clicks
-document.addEventListener("click", () => {
-    if (alphabeticalByNameRadioButton.checked) {
-        sortByNameAlphabetically();
-    }
-    if (alphabeticalByEmailRadioButton.checked) {
-        sortByEmailAlphabetically();
-    }
+// Filter by search bar value and print to page
+searchBar.addEventListener("keyup", (e) => {
+    employeeListContainer.innerHTML = "";
+    const searchString = e.target.value;
+    const filterData = employeeList.employees.filter((employee) => {
+        return (
+            employee.name.toLowerCase().includes(searchString.toLowerCase()) || 
+            employee.email.toLowerCase().includes(searchString.toLowerCase())
+        );
+    }).forEach((result) => {
+        const newDiv = document.createElement("div");
+		newDiv.innerHTML = `<p><strong>Name:</strong> ${result.name}<br><strong>Email:</strong> ${result.email}<p>`;
+		employeeListContainer.append(newDiv);
+    });
 });
 
 // Sort Alphabetically by Name
-const sortByNameAlphabetically = () => {
-    employeeList.employees.sort((a, b) => {
-        let employeeNameA = a.name.toUpperCase();
-        let employeeNameB = b.name.toUpperCase();
-        return (employeeNameA < employeeNameB ) ? -1 : (employeeNameA > employeeNameB) ? 1 : 0;
-    }); 
-    printEmployeesToPage();
-};
+alphabeticalByNameRadioButton.addEventListener("click", () => {
+    if (alphabeticalByNameRadioButton.checked) {
+        searchBar.value = "";
+        employeeList.employees.sort((a, b) => {
+            let employeeNameA = a.name.toUpperCase();
+            let employeeNameB = b.name.toUpperCase();
+            return (employeeNameA < employeeNameB ) ? -1 : (employeeNameA > employeeNameB) ? 1 : 0;
+        }); 
+        printEmployeesToPage();
+    }
+});
 
 // Sort Alphabetically by Email
-const sortByEmailAlphabetically = () => {
-    employeeList.employees.sort((a, b) => {
-        let employeeEmailA = a.email.toUpperCase();
-        let employeeEmailB = b.email.toUpperCase();
-        return (employeeEmailA < employeeEmailB ) ? -1 : (employeeEmailA > employeeEmailB) ? 1 : 0;
-    });
-    printEmployeesToPage();
-};
+alphabeticalByEmailRadioButton.addEventListener("click", () => {
+    if (alphabeticalByEmailRadioButton.checked) {
+        searchBar.value = "";
+        employeeList.employees.sort((a, b) => {
+            let employeeEmailA = a.email.toUpperCase();
+            let employeeEmailB = b.email.toUpperCase();
+            return (employeeEmailA < employeeEmailB ) ? -1 : (employeeEmailA > employeeEmailB) ? 1 : 0;
+        });
+        printEmployeesToPage();
+    }
+});
