3f1805106ea25ef42bae7cb6bfe25fd52648d741
Thomas Cooper <thomascooper@T-JR-MBP.fios-router.home>
2022-06-16 21:48:53 -0400

    Add filter for services provided.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 // "Database" of employees
 const employeeList = 
 {"employees":[  
-    {"name":"Shyam", "email":"shyamjaiswal@gmail.com"},  
-    {"name":"Bob", "email":"bob32@gmail.com"},  
-    {"name":"Jai", "email":"jai87@gmail.com"},
-    {"name":"Thomas", "email":"thomas@gmail.com"},
-    {"name":"Matt", "email":"matt@gmail.com"},
-    {"name":"Justin", "email":"justin79@gmail.com"},
-    {"name":"Mac", "email":"mac12@gmail.com"},
-    {"name":"Katie", "email":"C-katie@gmail.com"},
-    {"name":"Rich", "email":"rich70@gmail.com"},  
-    {"name":"Aaron", "email":"Baaron@gmail.com"}, 
-    {"name":"Charlie", "email":"charlie@gmail.com"}, 
-    {"name":"Zack", "email":"Azack21@gmail.com"}, 
-    {"name":"Timmy", "email":"Timmy@gmail.com"}, 
-    {"name":"Jessica", "email":"jess@gmail.com"}, 
-    {"name":"Tim", "email":"Timmy@gmail.com"}, 
+    {"name":"Shyam", "email":"shyamjaiswal@gmail.com", "service":"Bathroom, HVAC, Kitchen"},  
+    {"name":"Bob", "email":"bob32@gmail.com", "service":"Bathroom, HVAC, Kitchen"},  
+    {"name":"Jai", "email":"jai87@gmail.com", "service":"Bathroom, Kitchen"},
+    {"name":"Thomas", "email":"thomas@gmail.com", "service":"Bathroom, HVAC, Kitchen"},
+    {"name":"Matt", "email":"matt@gmail.com", "service":"Bathroom, HVAC, Kitchen"},
+    {"name":"Justin", "email":"justin79@gmail.com", "service":"Bathroom"},
+    {"name":"Mac", "email":"mac12@gmail.com", "service":"Bathroom"},
+    {"name":"Katie", "email":"C-katie@gmail.com", "service":"Bathroom"},
+    {"name":"Rich", "email":"rich70@gmail.com", "service":"Bathroom"},  
+    {"name":"Aaron", "email":"Baaron@gmail.com", "service":"Bathroom"}, 
+    {"name":"Charlie", "email":"charlie@gmail.com", "service":"HVAC, Kitchen"}, 
+    {"name":"Zack", "email":"Azack21@gmail.com", "service":"HVAC, Kitchen"}, 
+    {"name":"Timmy", "email":"Timmy@gmail.com", "service":"Kitchen"}, 
+    {"name":"Jessica", "email":"jess@gmail.com", "service":"HVAC, Kitchen"}, 
+    {"name":"Tim", "email":"Timmy@gmail.com", "service":"Kitchen"}, 
 ]};
 
 const employeeListContainer = document.getElementById("employee-list-container");
 const searchBar = document.getElementById("search-bar");
 const alphabeticalByNameRadioButton = document.getElementById("alphabetical-name");
 const alphabeticalByEmailRadioButton = document.getElementById("alphabetical-email");
+const serviceRadioButton = document.getElementsByClassName("service");
 
 // Print employees to page
 const printEmployeesToPage = (employeeListArray) => {
     employeeListContainer.innerHTML = "";
 	employeeListArray.forEach((element) => {
 		const newDiv = document.createElement("div");
-		newDiv.innerHTML = `<p><strong>Name:</strong> ${element.name}<br><strong>Email:</strong> ${element.email}<p>`;
+		newDiv.innerHTML = `<p class="employee-container"><strong>Name:</strong> ${element.name}<br>
+                            <strong>Email:</strong> ${element.email}<br>
+                            <strong>Services Provided:</strong> ${element.service}<p>`;
 		employeeListContainer.append(newDiv);
 	});
 }
@@ -71,5 +74,19 @@ alphabeticalByEmailRadioButton.addEventListener("click", () => {
             return (employeeEmailA < employeeEmailB ) ? -1 : (employeeEmailA > employeeEmailB) ? 1 : 0;
         });
         printEmployeesToPage(employeeList.employees);
+    }
+});
+
+// Filter by Service
+document.addEventListener("click", (e) => {
+    if (e.target.name === "service") {
+        employeeListContainer.innerHTML = "";
+        const searchString = e.target.value;
+        const filteredByServiceData = employeeList.employees.filter((employee) => {
+            return (
+                employee.service.includes(searchString)
+            )
+        });
+        printEmployeesToPage(filteredByServiceData);
     }
 });
